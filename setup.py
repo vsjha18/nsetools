1a1319b43c42c57b9e0c120facdfd77e29151ff0
Vivek Jha <vsjha18@gmail.com>
2015-01-01 23:19:54 +0530

    added setup.py file for this project

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, find_packages
+
+readme = 'sample readme'
+
+setup(
+    name="nsetools",
+    version="0.1.0",
+    author="Vivek Jha",
+    author_email="vsjha18@gmail.com",
+    description = ("Python library for extracting realtime data from National Stock Exchange"),
+    license="MIT",
+    keywords="nse quote market",
+    url ="http://vsjha18.github.com/nsetools",
+    packages = find_packages(),
+    long_description=readme,
+)
+
