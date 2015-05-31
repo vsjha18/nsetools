386ec23117f227598116ebf038781c5c7af07dc4
Vivek Jha <vsjha18@gmail.com>
2015-05-31 17:29:13 +0530

    code completion for 1.0.0

--- setup.py
+++ setup.py
@@ -1,11 +1,47 @@
 import os
 from setuptools import setup, find_packages
 
-readme = 'sample readme'
+readme = '''
+Project Page
+=============
+http://vsjha.in/nsetools
+
+nsetools
+========
+
+Python library for extracting realtime data from National Stock Exchange (India)
+
+Introduction.
+============
+
+nsetools is a library for collecting real time data from National Stock Exchange (India). It can be used in various types of projects which requires getting live quotes for a given stock or index or build large data sets for further data analytics. You can also build cli applications which can provide you live market details at a blazing fast speeds, much faster that the browsers. The accuracy of data is only as correct as provided on www.nseindia.com.
+
+Main Features:
+    =============
+
+    * Getting live quotes for stocks using stock codes.
+    * Return data in both json and python dict and list formats.
+    * Getting quotes for all the indices traded in NSE, e.g CNX NIFTY, BANKNIFTY
+    * etc.
+    * Getting list of top losers.
+    * Getting list of top gainers.
+    * Helper APIs to check whether a given stock code or index code is correct.
+    * Getting list of all indices and stocks.
+    * Cent percent unittest coverage.
+
+    Dependencies
+    =============
+    To keep it simple and supported on most of the platforms, it uses only core python libraries, hence there are no external dependencies. It can be used out of box and absolutely not set up is required except an internet connection.
+
+    Detailed Documenation
+    =====================
+
+    For complete documenation, please refer http://vsjha.in/nsetools
+'''
 
 setup(
     name="nsetools",
-    version="0.2.0",
+    version="1.0.0",
     author="Vivek Jha",
     author_email="vsjha18@gmail.com",
     description = ("Python library for extracting realtime data from National Stock Exchange"),
@@ -15,4 +51,3 @@ setup(
     packages = find_packages(),
     long_description=readme,
 )
-
