e500a3c3bfbd718fe2cb57cb4fc869bc4afa4ed0
Vivek Jha <vsjha18@gmail.com>
2017-12-15 00:46:33 +0530

    creds check

--- nse.py
+++ nse.py
@@ -345,6 +345,7 @@ class Nse(AbstractBaseExchange):
 if __name__ == "__main__":
     n = Nse()
     data = n.download_bhavcopy("14th Dec")
+
 # TODO: get_most_active()
 # TODO: get_top_volume()
 # TODO: get_peer_companies()
