80692a9c4aae686ac1c17169fcf1244e5394e42d
Swapniel <swapniel99@gmail.com>
2019-08-24 22:12:33 +0530

    bugfix in test.

--- tests/nse_tests.py
+++ tests/nse_tests.py
@@ -209,7 +209,7 @@ class TestCoreAPIs(unittest.TestCase):
 
     def test_6th_Dec_1994(self):
         data = self.nse.download_bhavcopy('1994-12-06')
-        self.assertIsInstance(self, data, bytes)
+        self.assertIsInstance(data, bytes)
 
     def test_top_fno_gainers_losers(self):
         fno_gainer = self.nse.get_top_fno_gainers()
