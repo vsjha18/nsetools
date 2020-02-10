c9be6902101a1bb4a08f0d751548cf31196abe3d
Kyle Altendorf <sda@fstab.net>
2020-02-10 15:02:46 -0500

    Fill out setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,5 @@
 from setuptools import setup, find_packages
 
-readme = '''
-Project Page
-=================
-http://nsetools.readthedocs.io
-
-nsetools
-=================
-Python library for extracting realtime data from National Stock Exchange (India)
-
-Introduction.
-=================
-nsetools is a library for collecting real time data from National Stock Exchange (India). It can be used in various types of projects which requires getting live quotes for a given stock or index or build large data sets for further data analytics. You can also build cli applications which can provide you live market details at a blazing fast speeds, much faster that the browsers. The accuracy of data is only as correct as provided on www.nseindia.com.
-
-Main Features:
-==================
-
-    * Getting live quotes for stocks using stock codes.
-    * Return data in both json and python dict and list formats.
-    * Getting quotes for all the indices traded in NSE, e.g CNX NIFTY, BANKNIFTY
-    * etc.
-    * Getting list of top losers.
-    * Getting list of top gainers.
-    * Helper APIs to check whether a given stock code or index code is correct.
-    * Getting list of all indices and stocks.
-    * Cent percent unittest coverage.
-
-Dependencies
-===================
-To keep it simple and supported on most of the platforms, it uses only core python libraries, hence there are no external dependencies. It can be used out of box and absolutely not set up is required except an internet connection.
-
-Detailed Documenation
-========================
-For complete documenation, please refer http://nsetools.readthedocs.io
-'''
-
 setup(
     name="nsetools",
     version="1.0.11",
@@ -43,8 +8,15 @@ setup(
     description="Python library for extracting realtime data from National Stock Exchange",
     license="MIT",
     keywords="nse quote market",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    python_requires=">=3",
     install_requires=['six', 'dateutils'],
     url="http://vsjha18.github.com/nsetools",
     packages=find_packages(),
-    long_description=readme,
+    long_description=pathlib.Path('README.md').read_text('utf-8'),
+    long_description_content_type="text/markdown",
 )
