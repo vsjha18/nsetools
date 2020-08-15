111ffe4e6e938efc7697635bbb3da597b12b023b
asquare100 <abhinav.ankur100@gmail.com>
2020-08-15 14:52:28 +0530

    Added basic futures quote

--- nse.py
+++ nse.py
@@ -57,6 +57,7 @@ class Nse(AbstractBaseExchange):
         self.headers = self.nse_headers()
         # URL list
         self.get_quote_url = 'https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?'
+        self.get_derivative_quote_url = 'https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuoteFO.jsp?'
         self.stocks_csv_url = 'http://www1.nseindia.com/content/equities/EQUITY_L.csv'
         self.top_gainer_url = 'http://www1.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json'
         self.top_loser_url = 'http://www1.nseindia.com/live_market/dynaContent/live_analysis/losers/niftyLosers1.json'
@@ -153,6 +154,45 @@ class Nse(AbstractBaseExchange):
         code = code.upper()
         if self.is_valid_code(code):
             url = self.build_url_for_quote(code)
+            req = Request(url, None, self.headers)
+            # this can raise HTTPError and URLError, but we are not handling it
+            # north bound APIs should use it for exception handling
+            res = self.opener.open(req)
+
+            # for py3 compat covert byte file like object to
+            # string file like object
+            res = byte_adaptor(res)
+            res = res.read()
+            # Now parse the response to get the relevant data
+            match = re.search(\
+                        r'<div\s+id="responseDiv"\s+style="display:none">(.*?)</div>',
+                        res, re.S
+                    )
+            try:
+                buffer = match.group(1).strip()
+                # commenting following two lines because now we are not using ast and instead
+                # relying on json's ability to do parsing. Should be much faster and more
+                # reliable. 
+                #buffer = js_adaptor(buffer)
+                #response = self.clean_server_response(ast.literal_eval(buffer)['data'][0])
+                response = self.clean_server_response(json.loads(buffer)['data'][0])
+            except SyntaxError as err:
+                raise Exception('ill formatted response')
+            else:
+                return self.render_response(response, as_json)
+        else:
+            return None
+
+    def get_futures_quote(self, code, as_json=False):
+        """
+        gets the quote of futures derivative for a given stock code
+        :param code:
+        :return: dict or None
+        :raises: HTTPError, URLError
+        """
+        code = code.upper()
+        if self.is_valid_code(code):
+            url = self.build_url_for_futures_quote(code)
             req = Request(url, None, self.headers)
             # this can raise HTTPError and URLError, but we are not handling it
             # north bound APIs should use it for exception handling
@@ -379,6 +419,18 @@ class Nse(AbstractBaseExchange):
             return self.get_quote_url + encoded_args
         else:
             raise Exception('code must be string')
+
+    def build_url_for_futures_quote(self, code):
+        """
+        builds a url which can be requested for futures dericative of given stock code
+        :param code: string containing stock code.
+        :return: a url object
+        """
+        if code is not None and type(code) is str:
+            encoded_args = urlencode([('underlying', code), ('instrument', 'FUTSTK')])
+            return self.get_derivative_quote_url + encoded_args
+        else:
+            raise Exception('code must be string')  
 
     def clean_server_response(self, resp_dict):
         """cleans the server reponse by replacing:
